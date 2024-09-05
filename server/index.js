ecf62687deea03e7267bfc04ffdab2990e4fee38
Muhammad Kashif <144331636+0qasim@users.noreply.github.com>
2024-09-05 11:34:50 +0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ app.use(express.static("public")); //static
 
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["https://navigation-images-frontend.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
