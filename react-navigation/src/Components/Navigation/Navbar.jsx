a805d33257aed631202e58179d835a831c65377a
Muhammad Kashif <144331636+0qasim@users.noreply.github.com>
2024-09-06 07:28:57 +0500

    Update Navbar.jsx

--- react-navigation/src/Components/Navigation/Navbar.jsx
+++ react-navigation/src/Components/Navigation/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext,useEffect, useState } from "react";
 import styled from "styled-components";
 import { Link, useNavigate } from "react-router-dom";
 import { FaBars } from "react-icons/fa";
